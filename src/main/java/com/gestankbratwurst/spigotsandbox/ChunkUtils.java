8d0d9799acb37db4a1619e4bdc224f69c03cc147
Flo0 <unconfigured@null.spigotmc.org>
2021-09-08 19:59:49 +0200

    Some code cleanup

---
+++ src/main/java/com/gestankbratwurst/spigotsandbox/ChunkUtils.java
@@ -0,0 +1,10 @@
+package com.gestankbratwurst.spigotsandbox;/*******************************************************
+ * Copyright (C) Gestankbratwurst suotokka@gmail.com
+ *
+ * This file is part of SpigotSandbox and was created at the 08.09.2021
+ *
+ * SpigotSandbox can not be copied and/or distributed without the express
+ * permission of the owner.
+ *
+ */public class ChunkUtils {
+}
